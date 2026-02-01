45d807164f51c74900f47b2fe9efe5e77d1d7b9f
lucasmessner <mobil.messner@gmail.com>
2026-02-01 13:06:06 +0100

    Update run.sh

--- actual_budget/run.sh
+++ actual_budget/run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/with-contenv bashio
-echo "Starting Actual Budget server..."
 
-# Minimal Beispiel: hier könntest du den echten Container starten
-# python3 HTTP Server für Test
-python3 -m http.server 5006
+echo "Starting Actual Budget Add-on..."
+python3 -m http.server 3000
