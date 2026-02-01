c9d5e732c7bbfc82673a8d91279667ad272482e7
lucasmessner <mobil.messner@gmail.com>
2026-02-01 12:49:34 +0100

    Create run.sh

---
+++ addon/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+echo "Starting Actual Budget server..."
+
+# Minimal Beispiel: hier könntest du den echten Container starten
+# python3 HTTP Server für Test
+python3 -m http.server 5006
